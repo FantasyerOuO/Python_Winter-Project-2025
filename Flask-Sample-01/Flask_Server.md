575550c96442b0162dd3cf8c686d37bbd7ab5f47
FantasyerOuO <sungx12097@gmail.com>
2025-02-21 16:57:06 +0800

    今天上課的內容和我瞎搞的東西

---
+++ Flask-Sample-01/Flask_Server.md
@@ -0,0 +1,8 @@
+執行Flask APP，使其運行在本地電腦
+(http://127.0.0.1:5000)
+
+# 一般模式
+flask --app 應用程式檔案名稱 (不用.py) run
+
+# Debug 模式
+flask --app 應用程式檔案名稱 (不用.py) run --debug
---
+++ Flask-Sample-01/OpenAI_SDK_01_FlaskTest.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+client = OpenAI()
+
+def ChatGPT(System_Prompt, User_Message, Completion_model="gpt-4o-mini") :
+    completion = client.chat.completions.create(
+        model=Completion_model,
+        messages=[
+            {
+                "role": "system", 
+                "content": System_Prompt
+            },
+            {
+                "role": "user",
+                "content": User_Message
+            }
+        ]
+    )
+    response = completion.choices[0].message.content
+    return response
+
+
+if __name__ == "__main__":
+    System_Prompt, User_Message = input("設定一個和你對話的身分:"), input("你想對這個身分說的話:")
+    ChatGPT(System_Prompt=System_Prompt, User_Message=User_Message)
