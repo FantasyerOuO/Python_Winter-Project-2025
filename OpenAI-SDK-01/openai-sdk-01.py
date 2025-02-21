4149acc23d458ed540e9eeadf173dfd84f8eac8a
FantasyerOuO <sungx12097@gmail.com>
2025-02-21 10:51:30 +0800

    新增Flask

--- OpenAI-SDK-01/openai-sdk-01.py
+++ OpenAI-SDK-01/openai-sdk-01.py
@@ -1,18 +1,23 @@
 from openai import OpenAI
 client = OpenAI()
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {
-            "role": "system", 
-            "content": "你現在是一個電影售票員"
-        },
-        {
-            "role": "user",
-            "content": "請問現在還有復仇者聯盟1的票嗎?"
-        }
-    ]
-)
+def ChatGPT(System_Prompt, User_Message, Completion_model="gpt-4o-mini") :
+    completion = client.chat.completions.create(
+        model=Completion_model,
+        messages=[
+            {
+                "role": "system", 
+                "content": "{System_Prompt}".format(System_Prompt=System_Prompt)
+            },
+            {
+                "role": "user",
+                "content": "{User_Message}".format(User_Message=User_Message)
+            }
+        ]
+    )
+    print(completion.choices[0].message)
 
-print(completion.choices[0].message)
+
+if __name__ == "__main__":
+    System_Prompt, User_Message = input("設定一個和你對話的身分:"), input("你想對這個身分說的話:")
+    ChatGPT(System_Prompt=System_Prompt, User_Message=User_Message)
